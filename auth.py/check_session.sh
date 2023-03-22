9937d0144bcd947577c325e6af5701d7f6e47c3f
root <root@prod-01.kdlp.underground.software>
2023-03-22 16:18:50 -0400

    remove auth.py from repo
    Signed-off-by: root <root@prod-01.kdlp.underground.software>

--- auth.py/check_session.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -z "${1}" ]; then
-	uwsgi_curl  \
-		-H "Content-Type: application/x-www-form-urlencoded" \
-		-X GET 127.0.0.1:9090 \
-		"/check?token="${1}
-fi
--- auth.py/logout_session.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -z "${1}" ]; then
-	uwsgi_curl \
-		-H "Content-Type: application/x-www-form-urlencoded" \
-		-X GET 127.0.0.1:9090 \
-		"/logout?username="${1}
-fi
